db2817437c3f869f893715d36c1fc75c3baf0277
whitneybolar <43798491+whitneybolar@users.noreply.github.com>
2022-06-22 17:00:41 -0700

    new set up

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,27 @@
 // Everything OK?
 console.log('OK');
 
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
 // menu animaton 
 // https://codepen.io/alvarotrigo/pen/oNGzoYd
 function menuOnClick() {
@@ -47,7 +68,7 @@ $(document).ready(function() {
 //footer copyright
 
 // Get current year
-var today = new Date();
+/*var today = new Date();
 
 // Store current year
 var year = today.getFullYear();
@@ -57,5 +78,5 @@ console.log( today, year );
 
 
 // Show current year
-$('#copyright span').text( year );  
+$('#copyright span').text( year ); */ 
 
