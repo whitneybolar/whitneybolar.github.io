6efe4ed0b2a8a5855057c393d773af5ec4d9ded9
whitneybolar <43798491+whitneybolar@users.noreply.github.com>
2022-06-24 14:48:13 -0700

    updated

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,24 +3,23 @@ console.log('OK');
 
 // Change style of navbar on scroll
 window.onscroll = function() {myFunction()};
-function myFunction() {
+/*function myFunction() {
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
     } else {
         navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
     }
-}
-
+}*/
 // Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
+/*function toggleFunction() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
     } else {
-        x.className = x.className.replace(" w3-show", "");
+        //x.className = x.className.replace(" ", "");
     }
-}
+}*/
 
 // menu animaton 
 // https://codepen.io/alvarotrigo/pen/oNGzoYd
@@ -78,5 +77,5 @@ console.log( today, year );
 
 
 // Show current year
-$('#copyright span').text( year ); */ 
+$('#copyright span').text( year );  */
 
