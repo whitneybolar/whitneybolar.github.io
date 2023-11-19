245c8f77502d63633471dc5a4de9e74738916b20
whitneybolar <43798491+whitneybolar@users.noreply.github.com>
2023-11-18 23:46:18 -0800

    attempt 1

--- js/nav.js
+++ js/nav.js
@@ -5,6 +5,7 @@
     /*const menuNavUnder = document.getElementsByClassName("list-part-under");*/
 
 function switchMenuButton(){
+    $(menuBackground).style.backgroundColor = "#363636";
 
    if(buttonIconHamburger.style.display == "none"){
         buttonIconClose.style.display = "none";
@@ -39,6 +40,8 @@ function navVisibility(){
 window.onscroll = function() {
 
     var currentPosition = window.scrollY;
+
+    $(menuBackground).style.backgroundColor = "#363636";
     
     if(currentPosition > 50){
         $(menuBackground).delay(500).css({ transform: 'scale(0.1)' });
